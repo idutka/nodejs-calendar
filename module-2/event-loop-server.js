e544b1c7b1973c5bec294203f9665a15b1913438
Ivan Dutka <>
2020-10-05 15:08:32 +0300

    Module - 2: Event Loop. Workers (Clean up)

--- module-2/event-loop-server.js
+++ module-2/event-loop-server.js
@@ -1,11 +1,9 @@
 const http = require('http');
 const {longComputation} = require('./utils');
 
-const REPEAT = 100;
-
 http.createServer((req, res) => {
   const start = new Date();
-  longComputation(REPEAT);
+  longComputation();
   res.writeHead(200);
   res.end(`${new Date() - start}\n`);
 }).listen(3000, () => console.log(`Listening on port 3000...`));
